f11c2c234baed1bca3252962bed89fe5abb16353
LinZong <353388639@qq.com>
2017-10-09 23:16:47 +0800

    Reconstitute some algorithm when processing Base62

--- Long2short/Base62Class.cs
+++ Long2short/Base62Class.cs
@@ -9,30 +9,30 @@ namespace Long2short
     class Base62Class
     {
         Dictionary<int, char> Base62Dic = new Dictionary<int, char>();
-        public Dictionary<int, char> CreateDic()
+        public Base62Class()
         {
-            int a = 0;
-            for (int i = 97; i <= 122; i++)
-            {
-                Base62Dic.Add(a, Convert.ToChar(i));
-                a++;
-            } // 0-25 is a-z
-            for (int i = 65; i <= 90; i++)
-            {
-                Base62Dic.Add(a, Convert.ToChar(i));
-                a++;
-            }//26-51 is A-Z
-            for (int i = 48; i <= 57; i++)
-            {
-                Base62Dic.Add(a, Convert.ToChar(i));
-                a++;
-            } //52-61 is 0-9
+               
+                int a = 0;
+                for (int i = 97; i <= 122; i++)
+               {
+                    Base62Dic.Add(a, Convert.ToChar(i));
+                    a++;
+               } // 0-25 is a-z
+                for (int i = 65; i <= 90; i++)
+                {
+                    Base62Dic.Add(a, Convert.ToChar(i));
+                    a++;
+                }//26-51 is A-Z
+                for (int i = 48; i <= 57; i++)
+                {
+                    Base62Dic.Add(a, Convert.ToChar(i));
+                    a++;
+                } //52-61 is 0-9
 
-            return Base62Dic;
         }
-
         public List<int> GetBase62(int input)
         {
+            
             List<int> Base62Value = new List<int>();
             while (input > 0)
             {
@@ -45,36 +45,38 @@ namespace Long2short
 
         public string ReturnShortUrl(int UrlId)
         {
+            
             string s = "";
             var base62result = GetBase62(UrlId);
-            Dictionary<int, char> Dic = CreateDic();
+            
             for (int i = 0; i < (6 - base62result.Count()); i++)
             {
-                s = s + Dic[0];
+                s = s + Base62Dic[0];
             }
             for (int i = base62result.Count(); i > 0; i--)
             {
-                s = s + Dic[base62result[i - 1]];
+                s = s + Base62Dic[base62result[i - 1]];
             }
-            Dic.Clear();
+            //Dic.Clear();
             return "http://zap.tech/" + s;
         }
 
-        public int GetUrlId(string OriShortUrl)
-        {
-            char[] KeyCharList = OriShortUrl.ToCharArray();
-            double id = 0;
-            double counter = KeyCharList.Length - 1;
-            Dictionary<int, char> Dic = CreateDic();
-            foreach (var i in KeyCharList)
-            {
-                var c = Dic.First(r => r.Value == i).Key;
-                id += (c) * Math.Pow(62, counter);
-                counter--;
-            }
-            Dic.Clear();
-            int intid = (Int32)id;//To prevent too large operating number,So use a convert.
-            return intid;
-        }
+        //public long GetUrlId(string OriShortUrl)
+        //{
+        //    char[] KeyCharList = OriShortUrl.ToCharArray();
+        //    double id = 0;
+        //    double counter = KeyCharList.Length - 1;
+        //    //Dictionary<int, char> Dic = CreateDic();
+        //    foreach (var i in KeyCharList)
+        //    {
+        //        var c = Base62Dic.First(r => r.Value == i).Key;
+        //        id += (c) * Math.Pow(62, counter);
+        //        counter--;
+        //    }
+        //    //Dic.Clear();
+
+        //    return (long)id;
+        //}
+        //这个东西是在没有数据库的时候通过62进制换算成10进制的东西,接上了数据库的话基本上就没用了.
     }
 }
--- Long2short/Program.cs
+++ Long2short/Program.cs
@@ -14,13 +14,8 @@ namespace Long2short
     {
         static void Main(string[] args)
         {
-            //中国有句话，叫闷声发大财，这是最好的.
-            //但是我想想觉得一句话不说也不好.
-            //ProcessShortId p = new ProcessShortId();
-            //p下有两个方法，GetLongId和GetShortId，前者用于服务器请求原始链接，后者用于用户或者批量生成短链接.
+            
         }
-
-
     }
 
     class ProcessShortId
@@ -29,10 +24,11 @@ namespace Long2short
         //Init important components
         Base62Class base62calc = new Base62Class();
         MongoClass mongotask = new MongoClass();
-
         public void GetShortId(long? urlid, string LongUrl)
         {
+            
             var collect = mongotask.InitDatabase("mongodb://127.0.0.1:27017", "long2short", "long2shortinfo");
+            //mongodb://username:password@serveraddress/databaseName  如果要鉴权的话就改改.
             long RealUrlid = urlid ?? mongotask.CountList(collect);
             bool status = mongotask.NewShortId(collect, Convert.ToInt32(RealUrlid), base62calc.ReturnShortUrl(Convert.ToInt32(RealUrlid)), LongUrl,urlid);
             if(!status)
